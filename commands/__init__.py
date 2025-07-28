29597528134ff79a2b5300422126ce471c87318d
Michie Adams <michiemadams@outlook.com>
2025-07-28 10:17:11 -0700

    Updated commands initializer with new commands.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,13 +1,24 @@
+from .advance_round import AdvanceRoundCmd
 from .club_list import ClubListCmd
 from .create_club import ClubCreateCmd
+from .create_tournament import CreateTournamentCmd
 from .exit import ExitCmd
+from .match_results import MatchResultsCmd
 from .noop import NoopCmd
+from .register_player import RegisterPlayerCmd
+from .report import TournamentReportCmd
+from .tournament_list import TournamentListCmd
 from .update_player import PlayerUpdateCmd
 
 __all__ = [
+    "AdvanceRoundCmd",
     "ClubCreateCmd",
+    "CreateTournamentCmd",
     "ExitCmd",
     "ClubListCmd",
+    "MatchResultsCmd",
     "NoopCmd",
     "PlayerUpdateCmd",
+    "RegisterPlayerCmd",
+    "TournamentReportCmd",
 ]
